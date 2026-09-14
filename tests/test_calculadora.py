4561d081364a2f732be259ee8a458dbdee765639
Kauane Bianca <biancaportellalima@@gmail.com>
2026-09-13 23:54:18 -0300

    test: adiciona testes da calculadora

---
+++ tests/test_calculadora.py
@@ -0,0 +1,22 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
+from calculadora import calcular_media, verificar_situacao
+
+
+def test_calcular_media():
+    assert calcular_media(8, 7, 9) == 8
+
+
+def test_aluno_aprovado():
+    assert verificar_situacao(8) == "Aprovado"
+
+
+def test_aluno_em_recuperacao():
+    assert verificar_situacao(6) == "Recuperação"
+
+
+def test_aluno_reprovado():
+    assert verificar_situacao(4) == "Reprovado"
